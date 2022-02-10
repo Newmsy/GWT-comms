13d659f43cf5df2c992ba3b03c94fd4387e48eea
AlexNewms <alex.newman2@kpmg.co.uk>
2022-02-10 11:53:49 +0000

    Adds bootstrap and header styling

--- react-app/src/Layout/layout.js
+++ react-app/src/Layout/layout.js
@@ -1,22 +1,37 @@
 import React from "react";
+import { Sidebar } from "./sidebar";
 
 export const Layout = (props) => {
   return (
     <div>
-      <div
-        style={{
-          width: "100vw",
-          height: 50,
-          backgroundColor: "#00338d",
-          paddingTop: 20,
-        }}
-      >
-        <p
-          style={{ color: "#fff", fontSize: 30, marginLeft: 50, marginTop: 0 }}
+      <div style={{ maxWidth: "100vw" }}>
+        <header
+          class="p-3 text-white"
+          style={{ backgroundColor: "#00338d", width: "100vw", padding: 0 }}
         >
-          Hybrid Calendar{" "}
-        </p>
+          <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+              <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li>
+                  <a href="#" class="nav-link px-2 text-white fs-3">
+                    <b>KPMG Hybrid Calendar</b>
+                  </a>
+                </li>
+              </ul>
+
+              <div class="text-end">
+                <button type="button" class="btn btn-light me-4">
+                  Create Event
+                </button>
+                <button type="button" class="btn btn-danger">
+                  Sign Out
+                </button>
+              </div>
+            </div>
+          </div>
+        </header>
       </div>
+
       {props.children}
     </div>
   );
