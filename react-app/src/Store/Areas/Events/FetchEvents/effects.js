b9692414f290d8684146efebb9f3c799a5d39ac3
AlexNewms <alex.newman2@kpmg.co.uk>
2022-02-11 11:16:35 +0000

    removed network requests

--- react-app/src/Store/Areas/Events/FetchEvents/effects.js
+++ react-app/src/Store/Areas/Events/FetchEvents/effects.js
@@ -5,14 +5,14 @@ import { ApiClient } from "../../../apiClient";
 function* fetchEventsWorker(action) {
   const apiClient = new ApiClient();
 
-  const response = yield apiClient.get("/api/CalendarEvent/all", false);
-  if (response?.ok) {
-    const data = yield response.json();
-    if (data.length > 0)
-      yield put(
-        getEventsActions.fetchEventsSuccess({ events: JSON.parse(data) })
-      );
-  }
+  // const response = yield apiClient.get("/api/CalendarEvent/all", false);
+  // if (response?.ok) {
+  //   const data = yield response.json();
+  //   if (data.length > 0)
+  //     yield put(
+  //       getEventsActions.fetchEventsSuccess({ events: JSON.parse(data) })
+  //     );
+  // }
 }
 
 export function* fetchEventsWatcher() {
